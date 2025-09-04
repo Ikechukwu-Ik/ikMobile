60e552dbb1caf52dd3025c3b9090961c0ba71f82
Ikechukwu-Ik <133080510+Ikechukwu-Ik@users.noreply.github.com>
2025-09-04 11:43:24 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,39 @@
+const toggleButton = document.getElementById("toggle-button");
+const menuList = document.getElementById("menu-list");
+const contactForm = document.getElementById("contact-form");
+
+
+
+toggleButton.addEventListener("click", function showMenu() {
+	menuList.classList.toggle("active");
+	toggleButton.classList.toggle("active");
+})
+
+document.querySelectorAll(".menu-item").forEach(n => n.addEventListener("click", () => {
+  menuList.classList.remove("active");
+  toggleButton.classList.remove("active");
+}))
+
+
+
+
+contactForm.addEventListener('submit', e => {
+    e.preventDefault();
+
+    var username = document.getElementById('username').value.trim();
+		var address = document.getElementById('address').value.trim();
+		var message = document.querySelector('.message-area').value.trim();
+
+    var approve = confirm ("Do you want to send this message to MU Shopping Mall?\n\n" + "From: " + username + "\nAddress: " + address + "\nContent: " + message);
+    if(approve == true) {
+    	alert("Your request has been submitted!")
+    }
+});
+
+
+
+
+////////////////////////////////////////////////////
+function showPopup() {
+	alert ("Message sent successfully.",);
+}
